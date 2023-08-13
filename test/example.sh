dc03554d6e131aedc775ec856ce90cdc866f5818
Jeremiah Faith <faithj02@Jeremiahs-MacBook-Air.local>
2023-08-13 19:03:35 -0400

    example should be working now for all 4 programs in the pipeline

--- test/example.sh
+++ test/example.sh
@@ -1,20 +1,28 @@
-echo 'TEST1: strain_detect paired-end'
-#time ../src/strain_detect -r Bacteroides_ovatus_1001283st1_B8_1001283B150210_160208.fna.gz -a Bacteroides_ovatus_1001283st1_B8_1001283B150210_160208.scrubbed_kmers.gz -b 1001283B150225_150804_H5_s07_small_PE1.fasta.gz -c 1001283B150225_150804_H5_s07_small_PE2.fasta.gz -t PE -o outfile.strain_detect.test.gz
-echo 'running diff to compare strain_detect paired-end output with expected result'
-diff outfile.strain_detect.gz outfile.strain_detect.test.gz
+echo ''
+echo 'STEP1: kmer_scrub_count (counting the frequency of target genome kmers in unrelated genomes and metagenomes to learn their frequency'
+echo 'example should take 1-3 minutes to run'
+../src/kmer_scrub_count -r strains/Bacteroides_ovatus_1001283st1_B8_1001283B150210_160208.fna.gz -A genomes_to_scrub.txt -B metagenomes_to_scrub.txt -p strains/Bacteroides_ovatus_1001283st1_B8_1001283B150210_160208.progress | gzip --best > strains/Bacteroides_ovatus_1001283st1_B8_1001283B150210_160208.scrub_kmer_counts.gz
+echo 'STEP1: complete'
 
 echo ''
 echo ''
-echo 'TEST2 strain_detect single-end'
-#time ../src/strain_detect -r Bacteroides_ovatus_1001283st1_B8_1001283B150210_160208.fna.gz -a Bacteroides_ovatus_1001283st1_B8_1001283B150210_160208.scrubbed_kmers.gz -b 1001283B150225_150804_H5_s07_small_PE1.fasta.gz -t SE -o outfile_SE.strain_detect.test.gz
-echo 'running diff to compare strain_detect single-end output with expected result'
-diff outfile_SE.strain_detect.gz outfile_SE.strain_detect.test.gz
+echo 'STEP2: keeping the 1% most rare kmers in the target genome'
+echo 'example should take <1 minute to run'
+python ../scripts/kmer_scrub_filter.py -s strains/Bacteroides_ovatus_1001283st1_B8_1001283B150210_160208.scrub_kmer_counts.gz -m 0.01 | gzip --best > strains/Bacteroides_ovatus_1001283st1_B8_1001283B150210_160208.scrubbed_kmers.gz
+echo 'STEP2: complete'
+
+echo ''
+echo ''
+echo 'STEP3: identifying informative (rare) kmers in the target metagenomes of interest'
+echo 'example should take <<1 minute to run'
+../src/strain_detect -r strains/Bacteroides_ovatus_1001283st1_B8_1001283B150210_160208.fna.gz -a strains/Bacteroides_ovatus_1001283st1_B8_1001283B150210_160208.scrubbed_kmers.gz -B target_metagenomes.txt -o strains/Bacteroides_ovatus_1001283st1_B8_1001283B150210_160208.kmer_hits.gz
+echo 'STEP3: complete'
 
 
 echo ''
 echo ''
-echo 'TEST3 kmer_scrub_count'
-# note we just grab the first 10K lines to keep file size small
-time ../src/kmer_scrub_count -r Bacteroides_ovatus_1001283st1_B8_1001283B150210_160208.fna.gz -A scrub_strain_list.txt -B scrub_metagenome_list.txt | head -n 10000 > outfile.scrub_kmer_counts.test
-echo 'running diff to compare scrub_kmer_counts output files'
-diff outfile.scrub_kmer_counts outfile.scrub_kmer_counts.test
+echo 'STEP4: counting the coverage and depth of the informative kmers that were found in each target metagenome'
+echo 'example should take <<1 minute to run'
+python ../scripts/coverage_depth.py -k strains/Bacteroides_ovatus_1001283st1_B8_1001283B150210_160208.kmer_hits.gz > strains/Bacteroides_ovatus_1001283st1_B8_1001283B150210_160208.coverage_depth
+echo 'STEP4: complete'
+echo ''
